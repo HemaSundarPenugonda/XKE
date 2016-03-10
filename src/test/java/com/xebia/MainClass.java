3924d9ea63b98194fba3b5ad534c6e3f99231de2
HemaSundarPenugonda <hemasundar@xebia.com>
2016-03-10 17:06:42 +0530

    Initial Commit
    Signed-off-by: HemaSundarPenugonda <hemasundar@xebia.com>

---
+++ src/test/java/com/xebia/MainClass.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.xebia;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+/**
+ * @author hemasundar
+ *
+ */
+public class MainClass {
+    /**
+     * 
+     */
+    @Test
+    public void method1() {
+	WebDriver driver = new FirefoxDriver();
+
+    }
+}
